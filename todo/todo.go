4084ba8d07065ef0bade9e9e96287604768f441e
Wilhelm Hansson <wilhelm.hansson@pm.me>
2023-08-31 15:48:06 +0200

    ADD sorting

--- todo/todo.go
+++ todo/todo.go
@@ -2,6 +2,7 @@ package todo
 
 import (
 	"fmt"
+	"sort"
 	"sync"
 	"try-htmx/pkg/utils"
 )
@@ -10,6 +11,20 @@ type TodoItem struct {
 	ID   string `form:"id"`
 	What string `form:"what"`
 	Done bool   `form:"done"`
+}
+
+type TodoItems []*TodoItem
+
+func (t TodoItems) Len() int {
+	return len(t)
+}
+
+func (t TodoItems) Less(i, j int) bool {
+	return t[i].ID < t[j].ID
+}
+
+func (t TodoItems) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
 }
 
 func (t *TodoItem) Valid() bool {
@@ -39,13 +54,27 @@ func NewTodoSVC() *TodoSVC {
 func (svc *TodoSVC) List() []*TodoItem {
 	svc.RLock()
 	defer svc.RUnlock()
-	ts := []*TodoItem{}
+	ts := TodoItems{}
 
 	for _, v := range svc.todos {
 		ts = append(ts, v)
 	}
 
+	sort.Sort(ts)
+
 	return ts
+}
+
+func (svc *TodoSVC) Get(id string) (*TodoItem, error) {
+	svc.RLock()
+	defer svc.RUnlock()
+	i, ok := svc.todos[id]
+	if !ok {
+		return nil, fmt.Errorf("no todo with id %s", id)
+	}
+
+	return i, nil
+
 }
 
 func (svc *TodoSVC) add(t *TodoItem) (string, error) {
